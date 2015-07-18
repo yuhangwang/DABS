105285784a7f92fb62454e1cb7ce4f8b8176078b
Yuhang Wang <stevenaura@outlook.com>
2015-07-18 15:38:39 -0500

    update license

--- src/python/plot/data/sample_data.py
+++
@@ -1,7 +0,0 @@
-"""
-Make sample data
-"""
-improt numpy
-
-list_output_files = ["data1.dat", "data2.dat", "data3.dat"]
-list_data = [[sin(x) for x in numpy.linspace], numpy.linspace(1,100,100), numpy.linspace(1,100,0.1)]
