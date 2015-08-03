f8b1e4797d2a2ee519f1986eaa00c825952cf060
Yuhang Wang <stevenaura@outlook.com>
2015-08-03 13:30:22 -0500

    update the architecture of get_rmsd.tcl

--- src/vmdtcl/measure/get_rmsd.tcl
+++ src/vmdtcl/measure/get_rmsd.tcl
@@ -1,41 +1,6 @@
-# Measure RMSD for the whole molecule
+# measure RMSD for the whole molecule
 # author: Yuhang Wang
-# date: 06/11/2015
-# note: since Tcl doesn't allow input argument to contain spaces
-#     the only way to work around is to use input files that
-#     contains an input string (with any number of spaces)
-# usage: vmd -dispdev text -e get_rmsd.tcl \
-#           -args PSF PDB DCD_PREFIX DCD_STAGE_START DCD_STAGE_END OUTPUT_PREFIX \
-#                 FILE_SELECTION_FOR_ALIGN FILE_SELECTION_FOR_RMSD
-
-puts "'------------'"
-set ccc 0
-set in_psf [lindex $argv $ccc]
-puts "in psf  $in_psf"
-incr ccc
-set in_pdb [lindex $argv $ccc]
-puts $in_pdb 
-
-incr ccc
-set in_dcd_prefix [lindex $argv $ccc]
-
-incr ccc
-set dcd_stage_one [lindex $argv $ccc]
-
-incr ccc
-set dcd_stage_end [lindex $argv $ccc]
-
-incr ccc
-set output_file_prefix [lindex $argv $ccc]
-
-incr ccc
-# a file containing an atom selection string for alignment
-set file_selection_for_align [lindex $argv $ccc]
-
-incr ccc
-# a file containing an atom selection string for the RMSD measurement
-set file_selection_for_rmsd [lindex $argv $ccc]
-
+# date: 08/03/2015
 #=================================================================
 proc get_selection_str {in_file_name} {
   ## Read a file and use its content a the returning string 
@@ -44,44 +9,50 @@ proc get_selection_str {in_file_name} {
   close $IN
   return $my_str
 }
-proc load {in_psf in_pdb in_dcd_prefix stage} {
-  ## load psf, pdb and dcd for stage i
-  set file_dcd "${in_dcd_prefix}${stage}.dcd"
+
+proc load {file_psf file_ref_pdb file_dcd} {
+  ## load psf, pdb and dcd 
   puts "\n\n\n"
   puts "=============================================================="
   puts "--------------------------------------------------------------"
-  puts "     Loading $in_psf"
-  puts "     Loading $in_pdb"
+  puts "     Loading $file_psf"
+  puts "     Loading $file_ref_pdb"
   puts "     Loading $file_dcd"
   puts "--------------------------------------------------------------"
-  set molId [mol new $in_psf]
-  mol addfile $in_pdb molid $molId 
+  set molId_ref    [mol new $file_ref_pdb]
+  set molId_target [mol new $file_psf]
  
-  mol addfile $file_dcd waitfor all molid $molId 
-  return $molId
+  mol addfile $file_dcd waitfor all ;# note: molid option doesn't work
+  return [list $molId_target $molId_ref]
 }
 
-proc align {molId str_selection_for_align} {
+proc align {molId_target molId_ref str_selection_for_align} {
   ## Align every frame to the 1st frame
 
   puts "--------------------------------------------------------------"
   puts "   Align the trajectory using this selection:"
   puts "   $str_selection_for_align"
   puts "--------------------------------------------------------------"
-  set ref [atomselect $molId $str_selection_for_align frame 0]
-  set totalNumFrames [molinfo $molId get numframes]
+  set atoms_align_ref     [atomselect $molId_ref $str_selection_for_align]
+  set atoms_align_target  [atomselect $molId_target $str_selection_for_align]
+  set atoms_to_move     [atomselect $molId_target all]
+  set totalNumFrames [molinfo $molId_target get numframes]
 
-  for {set i 1} {$i < $totalNumFrames} {incr i} {
-    set sel [atomselect $molId $str_selection_for_align frame $i]
-    set frame_i [atomselect $molId all frame $i]
-    $frame_i move [measure fit $sel $ref]
-    $sel delete
-    $frame_i delete
+  for {set i 0} {$i < $totalNumFrames} {incr i} {
+    $atoms_align_target frame $i
+    $atoms_to_move frame $i
+    $atoms_to_move move [measure fit $atoms_align_target $atoms_align_ref]
   }
-  $ref delete
+  set n1 [$atoms_align_ref num]
+  set n2 [$atoms_align_target num]
+  $atoms_align_target delete
+  $atoms_to_move delete
+  $atoms_align_ref delete
+  return [list $n1 $n2]
+
 }
 
-proc get_rmsd {molId str_selection} {
+proc get_rmsd {molId_target molId_ref str_selection} {
   # measure RMSD frame by frame 
   # relative to frame-0
   puts "--------------------------------------------------------------"
@@ -90,16 +61,17 @@ proc get_rmsd {molId str_selection} {
   puts "--------------------------------------------------------------"
   
   set list_rmsd {}
-  set ref [atomselect $molId $str_selection frame 0]
-  set totalNumFrames [molinfo $molId get numframes]
+  set atoms_rmsd_ref    [atomselect $molId_ref $str_selection]
+  set totalNumFrames    [molinfo $molId_target get numframes]
+  set atoms_rmsd_target [atomselect $molId_target $str_selection]
 
   for {set i 1} {$i < $totalNumFrames} {incr i} {
-    set sel [atomselect $molId $str_selection frame $i]
-    set data_rmsd [measure rmsd $sel $ref]
+    $atoms_rmsd_target frame $i
+    set data_rmsd [measure rmsd $atoms_rmsd_target $atoms_rmsd_ref]
     lappend list_rmsd $data_rmsd
-    $sel delete
   }
-  $ref delete
+  $atoms_rmsd_target delete
+  $atoms_rmsd_ref delete
   return $list_rmsd
 }
 
@@ -115,27 +87,46 @@ proc write_output {in_list output_file_name} {
   close $OUT
 }
 
+proc main {argv} {
+  puts "'------------'"
+  set ccc 0
+  set file_psf [lindex $argv $ccc]
+  puts "in psf  $file_psf"
+
+  incr ccc
+  set file_ref_pdb [lindex $argv $ccc]
+  puts $file_ref_pdb 
+
+  incr ccc
+  set file_dcd [lindex $argv $ccc]
+
+  incr ccc
+  set file_output [lindex $argv $ccc]
+
+  incr ccc
+  # a file containing an atom selection string for alignment
+  set file_selection_for_align [lindex $argv $ccc]
+
+  incr ccc
+  # a file containing an atom selection string for the RMSD measurement
+  set file_selection_for_rmsd [lindex $argv $ccc]
+
+  ## get atom selection string for alignment
+  set str_selection_for_align [get_selection_str $file_selection_for_align]
+
+  ## get atom selection string for RMSD calculation
+  set str_selection_for_rmsd [get_selection_str $file_selection_for_rmsd]
+
+  set list_molIds [load $file_psf $file_ref_pdb $file_dcd]
+  lassign $list_molIds molId_target molId_ref
+  puts "list molIds: $molId_target ; $molId_ref"
+  align $molId_target $molId_ref $str_selection_for_align
+  set list_rmsd [get_rmsd $molId_target $molId_ref $str_selection_for_rmsd]
+  write_output $list_rmsd $file_output
+  mol delete $molId_target 
+}
+
 #---------------------------------------------------------------------------
 #                     MAIN 
 #---------------------------------------------------------------------------
-
-## get atom selection string for alignment
-set str_selection_for_align [get_selection_str $file_selection_for_align]
-
-## get atom selection string for RMSD calculation
-set str_selection_for_rmsd [get_selection_str $file_selection_for_rmsd]
-
-set output_file_list "${output_file_prefix}-all.list"
-set OUT [open $output_file_list w]
-
-for {set s $dcd_stage_one} {$s <= $dcd_stage_end} {incr s} {
-  set molId [load $in_psf $in_pdb $in_dcd_prefix $s]
-  align $molId $str_selection_for_align
-  set list_rmsd [get_rmsd $molId $str_selection_for_rmsd]
-  set output_file_name "${output_file_prefix}${s}.dat"
-  puts $OUT $output_file_name
-  write_output $list_rmsd $output_file_name
-  mol delete $molId 
-}
-close $OUT
-exit
+main $argv; exit
