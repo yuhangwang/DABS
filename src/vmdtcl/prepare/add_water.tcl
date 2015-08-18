64d1cba2d2f6089f7a9126140856cfb6b8287a15
Yuhang Wang <stevenaura@outlook.com>
2015-08-18 09:42:54 -0500

    add an addition option for changing min_dist_water_solute in add_water.tcl

--- src/vmdtcl/prepare/add_water.tcl
+++ src/vmdtcl/prepare/add_water.tcl
@@ -2,7 +2,8 @@
 # >>>>>>>>>>>>> solvate <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 # Yuhang Wang
 # 06/10/15
-# usage: vmd -dispdev text -e add_water.tcl -args my.psf my.pdb my_out_prefix 100 100 100 [z offset]
+# usage: vmd -dispdev text -e add_water.tcl -args \
+# my.psf my.pdb my_out_prefix 100 100 100 [z offset] [min distance between water and solute]
 #========================================================
 
 package require solvate
@@ -19,25 +20,32 @@ set Ly [lindex $argv $ccc]
 incr ccc
 set Lz [lindex $argv $ccc]
 
-if {[llength $argv ] > 6} {
-	incr ccc
+incr ccc
+if {[llength $argv ] > $ccc} {
 	set z_offset [lindex $argv $ccc]
 } else {
 	set z_offset 0
 }
 
+incr ccc
+if {[llength $argv] > $ccc } {
+	set min_dist_between_water_solute [lindex $argv $ccc]
+} else {
+	# use default 
+	set min_dist_between_water_solute 2.4
+}
+
 set xmin [expr -$Lx/2.0]
 set ymin [expr -$Ly/2.0]
 set zmin [expr -$Lz/2.0 + $z_offset]
-set xmax [expr $Lx/2.0]
-set ymax [expr $Ly/2.0]
-set zmax [expr $Lz/2.0 + $z_offset]
+set xmax [expr  $Lx/2.0]
+set ymax [expr  $Ly/2.0]
+set zmax [expr  $Lz/2.0 + $z_offset]
 
 set list_min [list $xmin $ymin $zmin]
 set list_max [list $xmax $ymax $zmax]
 set list_minmax [list $list_min $list_max]
 
-set min_dist_between_water_solute 2.4
 solvate $in_psf $in_pdb -o  $output_prefix -b $min_dist_between_water_solute -minmax $list_minmax
 
 
